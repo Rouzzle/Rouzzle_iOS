58506611dc63a1ab8e76ea973b8b05f77eb088fe
kim dong kyung <kdkd422@naver.com>
2025-03-04 19:12:49 +0900

    Feat 커스텀 달력 구현

--- Rouzzle_iOS/Rouzzle_iOS/Features/Statistic/View/CalenderView.swift
+++ Rouzzle_iOS/Rouzzle_iOS/Features/Statistic/View/CalenderView.swift
@@ -8,11 +8,173 @@
 import SwiftUI
 
 struct CalendarView: View {
+    @State var month: Date = Date()
+    let weekdaySymbols: [String] = Calendar.current.shortWeekdaySymbols
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            headerView
+            calendarGridView
+            Spacer()
+        }
+    }
+    
+    private var headerView: some View {
+        VStack(alignment: .center) {
+            HStack {
+                Text("월간 요약")
+                    .font(.ptSemiBold(.headline))
+                Button {
+                    
+                } label: {
+                    Text("?")
+                }
+                Spacer()
+                
+                Button {
+                    changeMonth(by: -1)
+                } label: {
+                    Image(systemName: "chevron.left")
+                        .foregroundStyle(.basic)
+                }
+                
+                Text(month.formattedCalenderDate)
+                    .padding(.horizontal, 6)
+                
+                Button {
+                    changeMonth(by: 1)
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(.basic)
+                }
+                
+            }
+            HStack {
+                ForEach(weekdaySymbols.indices, id: \.self) { index in
+                    Text(weekdaySymbols[index].uppercased())
+                        .foregroundStyle(color(for: index))
+                        .frame(maxWidth: .infinity)
+                }
+            }
+        }
+    }
+    
+    private var calendarGridView: some View {
+        /// 현재 월에 존재하는 일 수
+        let daysInMonth: Int = numberOfDays(in: month)
+        /// 현재 달의 첫 날이 속한 요일을 0부터 시작하는 인덱스로 변환  = 3월 1일 토요일이므로 6 반환
+        let firstWeekday: Int = firstWeekdayOfMonth(in: month) - 1
+        /// 이전 달의 총 일 수
+        let lastDayOfMonthBefore = numberOfDays(in: previousMonth())
+        /// 현재 달의 날짜와 앞쪽의 빈 칸(첫 주의 여백)을 모두 포함하여 몇 행(row)이 필요한지 계산
+        let numberOfRows = Int(ceil(Double(daysInMonth + firstWeekday) / 7.0))
+        /// 그리드 전체 셀 수에서 현재 달의 날짜와 첫 주 빈 칸을 뺀 나머지 (다음 달의 보충 날짜)
+        let visibleDaysOfNextMonth = numberOfRows * 7 - (daysInMonth + firstWeekday)
+
+        return LazyVGrid(columns: Array(repeating: GridItem(), count: 7)) {
+            ForEach(-firstWeekday ..< daysInMonth + visibleDaysOfNextMonth, id: \.self) { index in
+                Group {
+                    if(index > -1 && index < daysInMonth) {
+                        let date = getDate(for: index)
+                        let day = Calendar.current.component(.day, from: date)
+                        let isToday = date.formattedDateToString == today.formattedDateToString
+                        
+                        Text("\(day)")
+                            .foregroundStyle(isToday ? .blue : .primary)
+                            .lineLimit(1)
+                            .padding(.vertical, 12)
+                    } else if let prevMonthDate = Calendar.current.date(
+                        byAdding: .day,
+                        value: index + lastDayOfMonthBefore,
+                        to: previousMonth()
+                    ) {
+                        let day = Calendar.current.component(.day, from: prevMonthDate)
+                        Text("\(day)")
+                            .foregroundStyle(.gray)
+                            .lineLimit(1)
+                            .padding(.vertical, 12)
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
     CalendarView()
+}
+
+private extension CalendarView {
+    var today: Date {
+        let now = Date()
+        let components = Calendar.current.dateComponents([.year, .month, .day], from: now)
+        return Calendar.current.date(from: components)!
+    }
+}
+
+/// 달력에 필요한 함수들 extension
+private extension CalendarView {
+    /// 요일 별 색상코드  함수
+    private func color(for index: Int) -> Color {
+        if index == 0 {
+            return .red
+        } else if index == weekdaySymbols.count - 1 {
+            return .blue
+        } else {
+            return .primary
+        }
+    }
+    
+    /// 월 바꾸기 함수
+    func changeMonth(by value: Int) {
+        self.month = Calendar.current.date(byAdding: .month, value: value, to: month) ?? month
+    }
+    
+    
+    /// 해당 월에 존재하는 일자 수
+    func numberOfDays(in date: Date) -> Int {
+        return Calendar.current.range(of: .day, in: .month, for: date)?.count ?? 0
+    }
+    
+    /// 인자로 받은  달의 첫 날이 속한 요일을 0부터 시작하는 인덱스로 변환
+    func firstWeekdayOfMonth(in date: Date) -> Int {
+        let components = Calendar.current.dateComponents([.year, .month], from: date)
+        let firstDayOfMonth = Calendar.current.date(from: components)!
+        
+        return Calendar.current.component(.weekday, from: firstDayOfMonth)
+    }
+    
+    /// 이전 달 Date 객체 반환
+    func previousMonth() -> Date {
+        let components = Calendar.current.dateComponents([.year, .month], from: month)
+        let firstDayOfMonth = Calendar.current.date(from: components)!
+        let previousMonth = Calendar.current.date(byAdding: .month, value: -1, to: firstDayOfMonth)!
+        
+        return previousMonth
+    }
+    
+    /// 특정 해당 날짜
+    func getDate(for index: Int) -> Date {
+      let calendar = Calendar.current
+      guard let firstDayOfMonth = calendar.date(
+        from: DateComponents(
+          year: calendar.component(.year, from: month),
+          month: calendar.component(.month, from: month),
+          day: 1
+        )
+      ) else {
+        return Date()
+      }
+      
+      var dateComponents = DateComponents()
+      dateComponents.day = index
+      
+      let timeZone = TimeZone.current
+      let offset = Double(timeZone.secondsFromGMT(for: firstDayOfMonth))
+      dateComponents.second = Int(offset)
+      
+      let date = calendar.date(byAdding: dateComponents, to: firstDayOfMonth) ?? Date()
+      return date
+    }
+    
 }
