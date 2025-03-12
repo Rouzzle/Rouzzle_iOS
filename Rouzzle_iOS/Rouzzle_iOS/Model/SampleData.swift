853c3debbb499f415f57408de1b26d07224e8f0c
kim dong kyung <kdkd422@naver.com>
2025-03-12 15:04:40 +0900

    Feat 테스트하기 위해 SwiftData MockData 생성하는 컨테이너 생성

--- Rouzzle_iOS/Rouzzle_iOS/Model/SampleData.swift
+++ Rouzzle_iOS/Rouzzle_iOS/Model/SampleData.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+class SampleData {
+    static let shared = SampleData()
+    
+    let modelContainer: ModelContainer
+    
+    //모델 컨테이너의 메인 컨텍스트에 접근할 수 있는 context
+    var context: ModelContext {
+        modelContainer.mainContext
+    }
+    
+    private init() {
+        let schema = Schema([
+            RoutineItem.self,
+            RoutineHistory.self,
+            TaskList.self
+        ])
+        
+        //디스크에 데이터를 저장하지 않고 메모리에만 저장 = isStoredInMemoryOnly = ture  빠르지만 임시적임
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+        
+        do {
+            modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }
+    
+    func insertSampleData() {
+        
+    }
+}
