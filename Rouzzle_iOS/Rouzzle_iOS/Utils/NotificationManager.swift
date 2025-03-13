091c0f859bc546722fbc0f4ad67a6d44770ffc6a
dardardardardar <180dayna@naver.com>
2025-03-13 19:12:27 +0900

    feat: NotificationManager파일 추가 (Utils/)

---
+++ Rouzzle_iOS/Rouzzle_iOS/Utils/NotificationManager.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationManager.swift
+//  Rouzzle_iOS
+//
+//  Created by 이다영 on 3/13/25.
+//
+
+import SwiftUI
+
+struct NotificationManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationManager()
+}
