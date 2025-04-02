0f2771fa24733c34029e7ae4b9081d4cbee48885
kim dong kyung <kdkd422@naver.com>
2025-04-02 17:47:08 +0900

    build project.pbxproj

---
+++ Rouzzle_iOS/Rouzzle_iOS/Features/Statistic/View/RoutineSummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineSummaryView.swift
+//  Rouzzle_iOS
+//
+//  Created by 김동경 on 4/2/25.
+//
+
+import SwiftUI
+
+struct RoutineSummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutineSummaryView()
+}
---
+++ Rouzzle_iOS/Rouzzle_iOS/Utils/Extension/RoutineItem+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  RoutineItem+Extension.swift
+//  Rouzzle_iOS
+//
+//  Created by 김동경 on 4/2/25.
+//
+
+import SwiftUI
+
+struct RoutineItem_Extension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoutineItem_Extension()
+}
