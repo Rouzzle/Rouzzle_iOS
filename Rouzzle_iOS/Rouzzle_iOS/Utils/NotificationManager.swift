557bb9712b5fa27b9e29ab8d1ad50e80829ea48b
dardardardardar <180dayna@naver.com>
2025-04-18 21:30:54 +0900

    feat: 주석 삭제

--- Rouzzle_iOS/Rouzzle_iOS/Utils/NotificationManager.swift
+++ Rouzzle_iOS/Rouzzle_iOS/Utils/NotificationManager.swift
@@ -28,11 +28,6 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
         return index == 0 ? "지금 바로 시작해볼까요?" : "\(index * intervalMinutes)분이 지났어요! 지금 시작해봐요"
     }
     
-//    private func createTrigger(for date: Date, repeats: Bool) -> UNCalendarNotificationTrigger {
-//        let components = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: date)
-//        return UNCalendarNotificationTrigger(dateMatching: components, repeats: repeats)
-//    }
-    
     /// 요일, 기준 시각, index 및 간격을 기반으로 주간 반복 트리거 생성 (옵셔널 안전 처리)
     private func createWeeklyTrigger(weekday: Int, baseHour: Int, baseMinute: Int, index: Int, intervalMinutes: Int) -> UNCalendarNotificationTrigger? {
         let calendar = Calendar.current
